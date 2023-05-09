c4ec6b7dd66f95a737dce8826b8d176b0e1c6fd8
JinSan-RM <yug6789@naver.com>
2023-05-09 15:54:38 +0900

    Add changes to jinsan branch

--- safezone/safezone/settings.py
+++ safezone/safezone/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+#mysql로 변경할 예정
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
