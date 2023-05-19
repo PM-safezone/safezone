d0908192a69a56d1baf47964a0a29619d9a9f2ec
leemoongi <moongi910423@gmail.com>
2023-05-19 17:11:56 +0900

    bugfix

--- safezone/account_app/decorators.py
+++ safezone/account_app/decorators.py
@@ -5,7 +5,7 @@ from account_app.models import UserModel
 def admin_ownership_required(func):
 	def decorated(request, *args, **kwargs):
 		user = UserModel.objects.get(pk=kwargs['pk'])
-		if user != request.user:
+		if user.pk != request.user.pk:
 			return HttpResponseForbidden()
 		return func(request, *args, **kwargs)
 	return decorated
