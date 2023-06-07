8afe5cc9e13820e3a51f17c332aabe3667b29d55
YONGHEELEE <leeyonghe90@gmail.com>
2023-06-07 10:36:38 +0900

    detect.py save results 수정

--- safezone/media/yolov5/detect.py
+++ safezone/media/yolov5/detect.py
@@ -244,25 +244,27 @@ def run(
 
             
             prev_frames.append(im0)  # im0를 prev_frames에 추가
-            # Save results (image with detections)
-            #if save_img:
-            #    if dataset.mode == 'image':
-            #        cv2.imwrite(save_path, im0)
-            #    else:  # 'video' or 'stream'
-            #        if vid_path[i] != save_path:  # new video
-            #            vid_path[i] = save_path
-            #            if isinstance(vid_writer[i], cv2.VideoWriter):
-            #                vid_writer[i].release()  # release previous video writer
-            #            if vid_cap:  # video
-            #                fps = vid_cap.get(cv2.CAP_PROP_FPS)
-            #                w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-            #                h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            #            else:  # stream
-            #                fps, w, h = 30, im0.shape[1], im0.shape[0]
-            #            save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
-            #            vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-            #        vid_writer[i].write(im0) 
-            
+                        
+            #Save results (image with detections)
+            if alarm == '':
+                if save_img:
+                    if dataset.mode == 'image':
+                        cv2.imwrite(save_path, im0)
+                    else:  # 'video' or 'stream'
+                        if vid_path[i] != save_path:  # new video
+                            vid_path[i] = save_path
+                            if isinstance(vid_writer[i], cv2.VideoWriter):
+                                vid_writer[i].release()  # release previous video writer
+                            if vid_cap:  # video
+                                fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                                w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                                h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                            else:  # stream
+                                fps, w, h = 30, im0.shape[1], im0.shape[0]
+                            save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
+                            vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'x264'), fps, (w, h))
+                        vid_writer[i].write(im0) 
+                
             
             # deque 만들기  
             if alarm == 'SMS':         
