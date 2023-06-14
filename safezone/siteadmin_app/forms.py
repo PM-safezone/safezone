0213139892c1467624bfe5e1b9fff6d8d1689f90
Yoo Bin <seoyoobin1743@gmail.com>
2023-06-15 00:23:19 +0900

    yoobin final UI edit

--- safezone/siteadmin_app/forms.py
+++ safezone/siteadmin_app/forms.py
@@ -4,61 +4,67 @@ from siteadmin_app.models import SiteAdmin
 
 
 class SiteAdminCreationForm(ModelForm):
-	name = forms.CharField(
-		widget=forms.TextInput(
-			attrs={
-				'class': 'name-container',
-				'type': 'text',
-				'placeholder': 'name',
-				'uname': 'uname',
-				'required': True
-			}
-		)
-	)
-	image = forms.ImageField(
-		required=True,
-		widget=forms.ClearableFileInput(
-			attrs={
-				'class': 'image-container'
-			}
-		)
-	)
-	MANAGEMENT_LOCATIONS_CHOICES = [
-		('placeholder', 'choice management location.'),  # placeholder
-		('option1', '구역 1'),
-		('option2', '구역 2'),
-		('option3', '구역 3'),
-	]
-	management_locations = forms.ChoiceField(
-		choices=MANAGEMENT_LOCATIONS_CHOICES,
-		widget=forms.Select(
-			attrs={
-				'class': 'ml-container ml-select',
-				'uname': 'ml',
-				'required': True,
-				'placeholder': 'choice management location.'
-			}
-		)
-	)
-	phone = forms.CharField(
-		widget=forms.TextInput(
-			attrs={
-				'class': 'phone-container',
-				'placeholder': 'phone number',
-				'uname': 'phone',
-				'required': True
-			}
-		)
-	)
-	class Meta:
-		model = SiteAdmin
-		fields = ['name', 'image', 'management_locations', 'phone']
+    name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'name-container',
+                'type': 'text',
+                'placeholder': 'Name',
+                'uname': 'uname',
+                'required': True
+            }
+        )
+    )
+    image = forms.ImageField(
+        required=True,
+        widget=forms.ClearableFileInput(
+            attrs={
+                'class': 'image-container'
+            }
+        )
+    )
+    MANAGEMENT_LOCATIONS_CHOICES = [
+        ('placeholder', 'Choose Your Location'),  # placeholder
+        ('option1', '구역 1'),
+        ('option2', '구역 2'),
+        ('option3', '구역 3'),
+        ('option3', '구역 4'),
+        ('option3', '구역 5'),
+        ('option3', '구역 6'),
+        ('option3', '구역 7'),
+        ('option3', '구역 8'),
+        ('option3', '구역 9'),
+    ]
+    management_locations = forms.ChoiceField(
+        choices=MANAGEMENT_LOCATIONS_CHOICES,
+        widget=forms.Select(
+            attrs={
+                'class': 'ml-container ml-select',
+                'uname': 'ml',
+                'required': True,
+                'placeholder': 'Choose Your Location'
+            }
+        )
+    )
+    phone = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'phone-container',
+                'placeholder': 'Phone Number',
+                'uname': 'phone',
+                'required': True
+            }
+        )
+    )
+    class Meta:
+        model = SiteAdmin
+        fields = ['name', 'image', 'management_locations', 'phone']
 
 
 
 
 class SiteAdminDetailForm(SiteAdminCreationForm):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		for field in self.fields:
-			self.fields[field].disabled = True
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].disabled = True
