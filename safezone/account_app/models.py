db20d6ef9a618bc568af4433f80c9bb8ac4fc10a
leemoongi <moongi910423@gmail.com>
2023-06-15 16:14:18 +0900

    final

--- safezone/account_app/models.py
+++ safezone/account_app/models.py
@@ -9,6 +9,12 @@ class UserModel(User):
 		('option1', '구역 1'),
 		('option2', '구역 2'),
 		('option3', '구역 3'),
+		('option3', '구역 4'),
+		('option3', '구역 5'),
+		('option3', '구역 6'),
+		('option3', '구역 7'),
+		('option3', '구역 8'),
+		('option3', '구역 9'),
 	]
 	management_locations = models.CharField(max_length=100, choices=MANAGEMENT_LOCATIONS_CHOICES)
 	phone = models.CharField(max_length=100)
